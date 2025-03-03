41f34cb55eba76ff3f6ed306ff760012351a98eb
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2025-03-03 09:50:36 +0100

    Remove /dev/tty

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-echo "This text contains the word 'foo', piped through 'tee /dev/tty'." | tee /dev/tty | grep -q "the word 'foo'" || { echo "Deployment failed"; exit 1; }
 echo "This text contains the word 'foo', piped through 'tee /dev/stdout'." | tee /dev/stdout | grep -q "the word 'foo'" || { echo "Deployment failed"; exit 1; }
